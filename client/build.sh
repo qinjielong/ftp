44f1b45b99ffa3f314d1b11df76aca57f1520ee1
qinjielong <qinjielong@126.com>
2021-06-15 04:33:48 -0400

    删除无用文件

--- client/build.sh
+++ client/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-g++ -c common/base.cpp ftp_client.cpp -fPIC -O3
+g++ -c common/base.cpp ftp_client.cpp -fPIC -O3 -Wall
  
-g++ -shared -o libftp.so base.o ftp_client.o
+g++ -shared -o libftp.so base.o ftp_client.o -Wall
 
-g++ -g -o main main.cpp -L ./ -lftp -O3
+g++ -g -o main main.cpp -L ./ -lftp -O3 -Wall
 
 rm ./*.o -rf
 
